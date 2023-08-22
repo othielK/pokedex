d8624ac3781945dff36147c2ff62b62a3f8fdad7
othiel <othiel.leith@gmail.com>
2023-08-22 16:57:49 +0200

    challenge3react

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,28 @@
 function PokemonCard() {
-
+    const pokemon = pokemonList[0];
+    // console.log(pokemon.name)
     return (
-        <figure>
-            <img
-                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-            />
-            <figcaption>bulbasaur</figcaption>
 
-        </figure>
+        <div>
+            <figure>
+                {pokemon.imgSrc === undefined ? "???" : <img src={pokemon.imgSrc} />}
+            </figure>
+            <h1>{pokemon.name}</h1>
+
+        </div>
 
     )
 }
+
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew",
+    },
+];
 
 export default PokemonCard
