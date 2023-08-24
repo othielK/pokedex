6b90c8c5e4ee0f6c26b0a0e5986a82b195bd8bcb
othiel <othiel.leith@gmail.com>
2023-08-24 22:31:47 +0200

    state1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
 import PokemonCard from "./components/PokemonCard"
 
 
-
-
 function App() {
 
   const pokemonList = [
     {
-      name: "Bulbasaur",
+      name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
     },
     {
       name: "mew",
     },
   ];
 
+
+
+
+
   return (
     <div>
       < PokemonCard {...pokemonList[0]} />
+      < PokemonCard {...pokemonList[1]} />
+      < PokemonCard {...pokemonList[2]} />
+      < PokemonCard {...pokemonList[3]} />
+
     </div>
 
   )
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react"
+import PropTypes from "prop-types";
 
-import PropTypes from "prop-types"
+// function PokemonCard({ name, imgSrc }) {
+//     return (
+//         <>
+//             <h1>{name}</h1>
+//             {imgSrc === undefined ? "???" : <img src={imgSrc} alt={name} />}
+//         </>
+//     )
+// }
 
 function PokemonCard({ name, imgSrc }) {
+    const [count, setCount] = useState(0)
+    const pokemonIndex = () => {
+        setCount(count + 1)
+    }
 
     return (
         <>
-            <h1>{name}</h1>
-            {imgSrc === undefined ? "???" : <img src={imgSrc} alt={name} />}
+            <div>
+                <h2>{name}</h2>
+                <img src={imgSrc} alt={name} />
+                <h3> {count}</h3>
+                <button onClick={pokemonIndex} >Next</button>
+                <button onClick={() => setCount(count - 1)}>Previous</button>
+            </div>
         </>
     )
 }
-
 PokemonCard.propTypes = {
     pokemonList: PropTypes.shape(
         {
